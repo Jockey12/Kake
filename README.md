a20fcc353dc2d4a5a56c9d5575115197a0b66e19
jockey12 <jockeyisthebest@gmail.com>
2021-01-13 21:48:21 -0500

    modified:   README.md 	modified:   functiontet.cpp

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Making a new C++ project let me know any ideas
 # 
 Updated: functiontet.cpp, include_files.h
+2021.1.13 9:47PM - Fixed functiontet.cpp
--- functiontet.cpp
+++ functiontet.cpp
@@ -1,9 +1,9 @@
-// Created by Jockey //
-// functiontet.cpp //
-/* DO Not edit or tweak unless you asked me */
-// will be changing the file name.//
-
+// Created by Jockey 
+// functiontet.cpp 
+// DO Not edit or tweak unless you asked me
+// will be changing the file name.
 #include "include_files.h"
+
 using namespace std;
 
 // Definition of user-defined function doPrint()
@@ -13,8 +13,6 @@ void doPrint() // doPrint() is the called function in this example
 }
 
 // makes a function called "textAnswer"
-// V  V  V  V  V  V
-
 void textAnswer() 
 {
     
@@ -31,31 +29,20 @@ void textAnswer()
             break;
 
         if(choice =='Y' || choice =='y'){
-            cout << " " << endl;
             cout << "Hello welcome to textAnswer\n";
             cin >> answert;
-            cout << "To begin press Y. " << endl;
-            cin >> answert;
-            
-            
-            
+            cout << "To begin press Y. " << endl;            
             break;
         }
-    else if(choice != 'N' && choice != 'n' && choice != 'Y' && choice != 'y'){
-        cout << "Error try again " << endl;
-        valid = true;        
-      }  
-    }  
-}
+        
+
+            }  
+          }
+     }
+
 
 void housePrint()
 {
-    int a, b;
-    int answerq;
-    a = 3;
-    b = 83;
-    answerq = a * b;
-    std::cout << answerq;
     std::cout << "House()\n";
 }
 
